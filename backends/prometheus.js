6b3a536af5fb0bce32123d515f794d45c3dfa13e
Harm Weites <harm@weites.com>
2016-12-02 22:35:27 +0100

    Implement health

--- backends/prometheus.js
+++ backends/prometheus.js
@@ -11,8 +11,9 @@ var prom_endpoint = process.env.VAMP_METRICS_ENDPOINT || config['vamp.metrics.en
 
 module.exports = function($this, query_options) {
     // Help:
-    // var query_options = {frontend: gateway.lookup_name};
+    // var query_options = {frontend: gateway.lookup_name, code: '5xx'};
     var frontend = query_options.frontend;
+    var code = query_options.code;
 
     var options = {
         uri: prom_endpoint + '/api/v1/query',
@@ -24,7 +25,20 @@ module.exports = function($this, query_options) {
 
     return {
         count: function(config) {
-            // TODO
+            var query = "sum(rate(haproxy_frontend_http_responses_total{frontend=\""+frontend+"\",code=\""+code+"\"}[10s])) by (frontend)";
+            options.qs = {query: query};
+            return _(rp(options)
+                .then(function(v) {
+                    var total = Math.round(v.data.result[0].value[1]);
+                    var healthy = total? 0 : 1;
+                    return {
+                        total: total,
+                        healthy: healthy
+                    };
+                })
+                .catch(function (err) {
+                    console.log(err);
+                }));
         },
         average: function(config) {
             var query = "sum(rate(haproxy_frontend_http_requests_total{frontend=\""+frontend+"\"}[10s])) by (frontend)";
