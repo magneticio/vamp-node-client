86dfdd40ca9918a55d65801f5e244f18f16c0d54
Szymon <szymon.okroj@merapar.com>
2019-04-03 15:23:44 +0200

    VE-610 percentiles rounding

--- elasticsearch_metrics.js
+++ elasticsearch_metrics.js
@@ -215,7 +215,7 @@ module.exports = function(api, options) {
         let total = response.hits.total;
         let percentiles = response.aggregations.agg.values;
         Object.keys(percentiles).map(function(key, index) {
-          percentiles[key] = percentiles[key] === "NaN" ? 0 : percentiles[key];
+          percentiles[key] = percentiles[key] === "NaN" ? 0 : Math.round(percentiles[key] * 100) / 100;
         });
         let returnValue = {
           total: total,
